acc4160ba3b1c73dab8840040420586fdecec937
Txai-HvA <txai.pereira.de.lacerda@hva.nl>
2021-11-12 12:32:08 +0100

    Added labels above bars. Changed some styling

--- lastfm_graphs/index.js
+++ lastfm_graphs/index.js
@@ -45,20 +45,26 @@ d3.json('./lastfm.json').then((json) => {
     //Sorts dataset from highest playCount to lowest playCount (so decending)
     data = data.sort(function(a, b) { return b.playCount - a.playCount });
     
+    //top 20
+    data = data.slice(0, 20); 
+
     update(data);
 });
+
+
+
 
 
 function update(new_data) {
   
   //update the scales
   xscale.domain([0, d3.max(new_data, (d) => d.playCount)]);
-  yscale.domain(new_data.map((d) => `${d.artistName} - ${d.songName}`)); //Mapping on artist and song name
+  yscale.domain(new_data.map((d, i) => `#${i+1} ${d.artistName} - ${d.songName}`)); //Mapping on artist and song name
 
   //render the axis
   g_xaxis.call(xaxis);
   g_yaxis.call(yaxis);
-
+  
   // Render the chart with new data
 
   // DATA JOIN
@@ -79,15 +85,15 @@ function update(new_data) {
 
   //quantizeScale, where bars have different colors depending on playCount
   let quantizeScale = d3.scaleQuantize()
-    .domain([0, 50])
+    .domain([0, 20])
     //.domain(d3.extent(data))//Takes the length of the dataset
       .range(['red', 'orange', 'yellow', 'green', "blue"]);
 
   rect
-    .attr('height', yscale.bandwidth())
+    .attr('height', yscale.bandwidth())//
     .transition()
       .ease(d3.easeElastic)//Animation when the amount of shown songs gets changed
-      .attr('y', (d) => yscale(`${d.artistName} - ${d.songName}`))
+      .attr('y', (d, i) => yscale(`#${i+1} ${d.artistName} - ${d.songName}`))
     .style('fill', function(d, index) {
       return quantizeScale(index);
     })
@@ -98,30 +104,41 @@ function update(new_data) {
       })
       .attr('width', (d) => xscale(d.playCount));
 
+
+
+
+
   // WIP add image next to the rect above
-  const image = g.selectAll("image")
+  const images = g.selectAll("image")
     .data(new_data)
     .enter().append("svg:image")
       .attr("xlink:href", (d) => d.image["#text"])//gets image url
-      .attr("x", -13)
-      .attr("y", (d) => yscale(`${d.artistName} - ${d.songName}`))
+      .attr("x", -40)
+      .attr("y", (d, i) => yscale(`#${i+1} ${d.artistName} - ${d.songName}`))
       .attr("width", yscale.bandwidth())
       .attr("height", yscale.bandwidth());
   //Bron http://bl.ocks.org/hwangmoretime/c2c7128c5226f9199f87
 
 
-
-  g.selectAll("text")
-  .data(new_data)
-  .enter().append(text)
-        .text(function(d) {
-          return d.playCount;
-        })
-          .attr("class", "countLabel")
-          .attr("x", 10)//
-          .attr("y", 0)
-  //http://jsfiddle.net/enigmarm/3HL4a/13/
+  // WIP add playCount labels above the bars (rect)
+  const labels = svg.selectAll(".text")        
+    .data(new_data)
+      .enter()
+        .append("text")
+          .attr("class","playCountLabel")
+          .attr("x", (d) => xscale(d.playCount) + 330)
+          .attr('y', (d, i) => yscale(`#${i+1} ${d.artistName} - ${d.songName}`) + 10)
+          .attr("dy", ".75em")
+          .transition().delay(function(d, i) {
+            return i * 50;
+          })
+            .style("opacity", 1)
+            .text((d) => d.playCount);
+          
 }
+
+
+
 
 
 
@@ -130,7 +147,7 @@ function update(new_data) {
 d3.select(filterAmountOfSongs).on("change", function() {
   const newSize = d3.select(this).property("value");  
     
-  if(newSize > 1 && newSize <= 50) {
+  if(newSize > 1 && newSize <= 20) {
       const filtered_data = data.slice(0, newSize); 
       update(filtered_data);
   } else {
@@ -141,33 +158,10 @@ d3.select(filterAmountOfSongs).on("change", function() {
 d3.select(filterAmountOfSongs).on("input", function() {
   const newSize = d3.select(this).property("value");  
     
-  if(newSize > 1 && newSize <= 50) {
+  if(newSize > 1 && newSize <= 20) {
       const filtered_data = data.slice(0, newSize); 
       update(filtered_data);
   } else {
       update(data); //Update the chart with all the data
   }
 });
-
-
-
-
-
-
-
-//Filters on a specific user
-// d3.select(filterUser).on("input", function() {
-//     const newUser = d3.select(this).property("value");
-//     //If the given user is in de dataset...
-//     if (data.filter(e => e.userName === newUser).length > 0) {
-//         const filtered_data = data.filter((d) => d.userName === newUser);
-//         update(filtered_data);
-//     } else {
-//         update(data); //Update the chart with all the data we have
-//     }
-// });
-
-//Filters on a specific period
-// d3.select(filterPeriod).on("input", function() {
-
-// });
